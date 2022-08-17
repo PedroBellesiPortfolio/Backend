7779fd0f3b4771ce501aa8fb29c9747b5abe82d7
Pedro Bellesi <pedrobellesi@gmail.com>
2022-08-16 21:45:22 -0300

    actualizacion ver usuario

--- src/main/java/com/portfolio/portfoliopedro/controller/UsuarioController.java
+++ src/main/java/com/portfolio/portfoliopedro/controller/UsuarioController.java
@@ -1,12 +1,17 @@
 
 package com.portfolio.portfoliopedro.controller;
 
+
 import com.portfolio.portfoliopedro.model.Usuario;
 import com.portfolio.portfoliopedro.repository.Usuariorepo;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,6 +22,15 @@ public class UsuarioController {
  
     @Autowired
     private Usuariorepo usurep;
+    
+    @CrossOrigin(origins = "*")
+    @GetMapping("/usuario/ver")  
+    @ResponseBody
+    public List<Usuario> listausuario(){
+     List<Usuario> listausuario = usurep.findAll();
+    return  listausuario;    
+    
+    }
    
     
     @PostMapping("/usuario/mas")
